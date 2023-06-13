6a5cd888be34554407df03386b5f36e7fa6a8453
peas-developer <peasdeveloper@gmail.com>
2023-06-13 22:40:52 +0700

    note-app versi 3
    penambahan DB dengan postgreSQL

--- note-app-v3/src/api/notes/handler.js
+++ note-app-v3/src/api/notes/handler.js
@@ -12,12 +12,12 @@ class NotesHandler{
         this.deleteNoteByIdHandler = this.deleteNoteByIdHandler.bind(this)
     }
 
-    postNoteHandler(req, h){
+    async postNoteHandler(req, h){
         try {
             this._validator.validateNotePayload(req.payload)
-            const {title = 'untitled', tags, body} = req.payload 
+            const {title , tags, body} = req.payload 
 
-            const noteId = this._service.addNote({title, tags, body})
+            const noteId = await this._service.addNote({title, tags, body})
     
             const response = h.response({
                 status : 'success',
@@ -35,7 +35,6 @@ class NotesHandler{
                   message: error.message,
                 });
                 response.code(error.statusCode);
-                console.log(error)
                 return response;
               }
          
@@ -50,8 +49,8 @@ class NotesHandler{
         }
     }
 
-    getNotesHandler(){
-        const notes = this._service.getNotes()
+    async getNotesHandler(){
+        const notes = await this._service.getNotes()
         return {
             status : 'success',
             data : {
@@ -60,10 +59,10 @@ class NotesHandler{
         }
     }
 
-    getNoteByIdHandler(req, h){
+    async getNoteByIdHandler(req, h){
         try {
             const {id} = req.params
-            const note = this._service.getNoteById(id)
+            const note = await this._service.getNoteById(id)
 
             return {
                 status : 'success',
@@ -78,7 +77,6 @@ class NotesHandler{
                   message: error.message,
                 });
                 response.code(error.statusCode);
-                console.log(error)
                 return response;
               }
          
@@ -93,12 +91,12 @@ class NotesHandler{
         }
     }
 
-    putNoteByIdHandler(req, h){
+    async putNoteByIdHandler(req, h){
         try {
             this._validator.validateNotePayload(req.payload)
             const {id} = req.params
 
-            this._service.editNoteById(id, req.payload)
+            await this._service.editNoteById(id, req.payload)
             
             return {
                 status : 'success',
@@ -111,7 +109,6 @@ class NotesHandler{
                   message: error.message,
                 });
                 response.code(error.statusCode);
-                console.log(error)
                 return response;
               }
          
@@ -126,11 +123,11 @@ class NotesHandler{
         }
     }
 
-    deleteNoteByIdHandler(req, h){
+    async deleteNoteByIdHandler(req, h){
         try {
             const {id} = req.params
 
-            this._service.deleteNoteById(id)
+            await this._service.deleteNoteById(id)
 
             return { 
                 status : 'success',
@@ -143,7 +140,6 @@ class NotesHandler{
                   message: error.message,
                 });
                 response.code(error.statusCode);
-                console.log(erroe)
                 return response;
               }
          
